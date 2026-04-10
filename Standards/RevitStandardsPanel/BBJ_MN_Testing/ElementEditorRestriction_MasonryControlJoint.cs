01660ae99369fa242421f40705a771154a6a09e9
T-Lane-00 <trey.lane@hok.com>
2026-04-10 16:50:48 -0500

    Update and rename ElementEditorRestriction.cs to ElementEditorRestriction_MasonryControlJoint.cs

--- Standards/RevitStandardsPanel/BBJ_MN_Testing/ElementEditorRestriction_MasonryControlJoint.cs
+++ Standards/RevitStandardsPanel/BBJ_MN_Testing/ElementEditorRestriction_MasonryControlJoint.cs
@@ -6,7 +6,7 @@ using System.IO;
 using System.Linq;
 using Autodesk.Revit.DB;
 
-public class ElementEditorRestriction  // ← matches filename
+public class ElementEditorRestriction_MasonryControlJoint  // ← matches filename
 {
     // ---------------------------------------------------------------
     // TESTING TOGGLE
